d7bb152d008ccac9022f9b11005e6e25c62c7976
Omo Agbagbara <joomla@deltastateonline.com>
2016-06-09 07:48:52 +1000

    Add constant variables

---
+++ app/config/constants.php
@@ -0,0 +1,8 @@
+<?php
+//file : app/config/constants.php
+
+return [
+		'ADMIN_NAME' => 'administrator',
+		'OUR_WEBSITE'=> 'app.deltastateonline.com',
+		'DESIGNED_BY'=> 'app.deltastateonline.com'
+];
